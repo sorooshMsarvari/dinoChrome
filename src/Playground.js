eb46f575e509b4fc7e94b13dc8b8a3f64ac12091
Soroosh Mirzasarvari <sarvisoroosh@gmail.com>
2022-04-21 01:11:49 +0430

    Change the width size from vw to px

--- src/Playground.js
+++ src/Playground.js
@@ -2,7 +2,7 @@ export class Playground {
   constructor(window_percentage) {
     let playground = document.createElement('div');
     playground.id = 'playground';
-    playground.style.width = window_percentage*100 + 'vw';
+    playground.style.width = window_percentage * window.screen.width + 'px';
     document.body.appendChild(playground);
   }
 }
