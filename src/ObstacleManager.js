6a9f0afaa901af710d78ff58356d237e74be5f3e
Soroosh Mirzasarvari <sarvisoroosh@gmail.com>
2022-04-24 00:40:37 +0430

    Add startMakingObstacle method to ObstacleManager

--- src/ObstacleManager.js
+++ src/ObstacleManager.js
@@ -1,11 +1,23 @@
 import { Cactus } from './Cactus.js';
-import {PLAYGROUND_END} from './DinaChrome';
+import {PLAYGROUND_END, OBSTACLE_GENERATION_RATE} from './DinaChrome.js';
 
 
 export class ObstacleManager {
   constructor(horizantal_ratio) {
     this.horizantal_ratio = horizantal_ratio;
     this.obstacle_array = [];
+  }
+
+  startMakingObstacle() {
+    this.make_new_obstacle();
+    this.obstacle_generation_interval = setInterval(
+      (function (self) {
+        return function () {
+          self.make_new_obstacle();
+        }
+      })(this),
+      OBSTACLE_GENERATION_RATE
+    );
   }
 
   make_new_obstacle() {
@@ -23,5 +35,5 @@ export class ObstacleManager {
   delete_obstacle() {
     let obs = this.obstacle_array.shift();
     obs.remove();
-  }
+  }  
 }
