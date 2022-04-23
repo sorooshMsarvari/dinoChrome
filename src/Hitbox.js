2e4e18ef9f4f9e7273f61b07ec22c6713d3a0df8
Soroosh Mirzasarvari <sarvisoroosh@gmail.com>
2022-04-23 23:50:43 +0430

    Add movement methods for Hitbox
    The whole moving mechanic will eventully get encapsulated in Hitbox

--- src/Hitbox.js
+++ src/Hitbox.js
@@ -1,7 +1,9 @@
 const SIZE_RATIO_TO_PX = 10;
 
 export class Hitbox {
-  constructor(htmlElement, width, height) {
+  constructor(htmlElement, width, height, x, y) {
+    this.x = x;
+    this.y = y;
     this.htmlElement = htmlElement;
     this.width = width;
     this.height = height;
@@ -15,4 +17,22 @@ export class Hitbox {
     div.appendChild(this.htmlElement);
     return div;
   }
+
+  moveUp(delta) {
+    this.y -= delta;
+  }
+
+  moveDown(delta) {
+    this.y += delta;
+  }
+
+  moveRight(delta) {
+    this.x += delta;
+  }
+
+  moveLeft(delta) {
+    this.x -= delta;
+  }
+
+  
 }
